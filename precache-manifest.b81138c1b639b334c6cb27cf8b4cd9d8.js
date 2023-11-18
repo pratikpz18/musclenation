c7dba9a39b925dab8b1278243c11bf5b15377824
pratikpz18 <zinjurdepratik.pz@gmail.com>
2023-11-18 22:24:01 +0530

    Updates

---
+++ precache-manifest.b81138c1b639b334c6cb27cf8b4cd9d8.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22fca3b6024f59b093b9853d3132d1a6",
+    "url": "/musclenation/index.html"
+  },
+  {
+    "revision": "49b5228bcd5938e093eb",
+    "url": "/musclenation/static/css/main.de31fb63.chunk.css"
+  },
+  {
+    "revision": "beda838ee4046a722b54",
+    "url": "/musclenation/static/js/2.3ab8df5b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/musclenation/static/js/2.3ab8df5b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49b5228bcd5938e093eb",
+    "url": "/musclenation/static/js/main.f93fef19.chunk.js"
+  },
+  {
+    "revision": "5c86f87446373a14cc8b",
+    "url": "/musclenation/static/js/runtime-main.f5831988.js"
+  },
+  {
+    "revision": "ae2508775e7b40b7f9678a27610e3578",
+    "url": "/musclenation/static/media/bgVideo.ae250877.mp4"
+  },
+  {
+    "revision": "dd1df8f478bb28433b5dfc6df4832f09",
+    "url": "/musclenation/static/media/classFour.dd1df8f4.jpg"
+  },
+  {
+    "revision": "5a6c841c01f957d21afe705bf1754ce0",
+    "url": "/musclenation/static/media/classOne.5a6c841c.jpg"
+  },
+  {
+    "revision": "3d605f688128d539f1898c2c8867814b",
+    "url": "/musclenation/static/media/classThree.3d605f68.jpg"
+  },
+  {
+    "revision": "a6ebee97d6a29927f91b2a238d1b8336",
+    "url": "/musclenation/static/media/classTwo.a6ebee97.jpg"
+  },
+  {
+    "revision": "31cb05f3e1c4697812b2104049d21c55",
+    "url": "/musclenation/static/media/contactBg.31cb05f3.jpg"
+  },
+  {
+    "revision": "f042c2ac7fe16211f6c2806e04fdebad",
+    "url": "/musclenation/static/media/gymOverlayBg.f042c2ac.jpg"
+  },
+  {
+    "revision": "02e364823438fedfc552123239968bf6",
+    "url": "/musclenation/static/media/scheduleBg.02e36482.jpg"
+  },
+  {
+    "revision": "7c2ab25d46f7a6f96ce234d934f56a80",
+    "url": "/musclenation/static/media/trainerOne.7c2ab25d.jpg"
+  },
+  {
+    "revision": "81e56a6dd10134e1affed0e5f247541f",
+    "url": "/musclenation/static/media/trainerThree.81e56a6d.jpg"
+  },
+  {
+    "revision": "db6c16448e4272b3e4eb88cdc84c356f",
+    "url": "/musclenation/static/media/trainerTwo.db6c1644.jpg"
+  }
+]);
